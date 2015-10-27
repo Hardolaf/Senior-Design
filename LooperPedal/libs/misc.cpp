68b80689b4c4a471dc5d12921a325336224f8b7b
Joseph Warner <hardolaf@gmail.com>
2015-10-27 16:31:51 -0400

    Add project file and fix misc.cpp

--- LooperPedal/libs/misc.cpp
+++ LooperPedal/libs/misc.cpp
@@ -17,7 +17,7 @@
 // -----------------------------------------------------------------------------
 void Delay_ms(unsigned int time)                     // Delay function in ms
 {
-  for(int i=0; i<time; i++)
+  for(unsigned int i=0; i<time; i++)
   {
     for(int h=0; h<420; h++){}
   }
